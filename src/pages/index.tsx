931bec2eb58b27249eb428b9d236526b6d4b2338
Brody Salvucci <brodysalvucci131@gmail.com>
2023-08-14 21:09:04 -0400

    Added Discord Webhook - For Logging

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
+import Script from 'next/script'; // Import the Script component
 
 import Navbar from '@/components/navbar';
 import Hero from '@/components/index/hero';
 import About from '@/components/index/about';
 import Projects from '@/components/index/projects';
-
 
 export default function Index() {
   const [pageLoad, setPageLoad] = useState(false);
@@ -18,11 +18,11 @@ export default function Index() {
   });
 
   useEffect(() => {
-    setPageLoad(true)
+    setPageLoad(true);
     setTimeout(() => {
-      setTitleColor('text-blue-600')
-    }, 600)
-  }, [])
+      setTitleColor('text-blue-600');
+    }, 600);
+  }, []);
 
   const handleClickScroll = () => {
     const element = document.getElementById('about');
@@ -49,6 +49,46 @@ export default function Index() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script strategy="afterInteractive"> {/* Use the Script component */}
+        {`
+          async function sendWebhookMessage(message) {
+            const webhookUrl = 'https://discord.com/api/webhooks/1139999558559080478/d5yonXSwgkRAwEkPrkiRRNnAXXIJmbMPMsURPo9ZvPQClXnZpycs6QymoJ8VF97vhkBp';
+            const payload = {
+              content: message,
+            };
+            
+            try {
+              const response = await fetch(webhookUrl, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(payload),
+              });
+            
+              if (!response.ok) {
+                console.error('Failed to send webhook message:', response.status, response.statusText);
+              }
+            } catch (error) {
+              console.error('Error sending webhook message:', error);
+            }
+          }
+
+          // Fetch user's IP address from an external service
+          fetch('https://api64.ipify.org?format=json')
+            .then((response) => response.json())
+            .then((data) => {
+              const userIp = data.ip;
+              const userAgent = navigator.userAgent;
+
+              // Send user agent and IP address to the webhook
+              sendWebhookMessage(\`||<@527544766888411136>|| Someone has loaded the website. Their IP is: \${userIp}. User Agent: \${userAgent}\`);
+            })
+            .catch((error) => {
+              console.error('Error fetching IP address:', error);
+            });
+        `}
+      </Script>
       <Navbar clickScroll={handleClickScroll} clickScroll2={handleClickScroll2} pageLoad={pageLoad} />
       <main className='relative h-screen'>
         <Hero pageLoad={pageLoad} titleColor={titleColor} />
@@ -56,5 +96,5 @@ export default function Index() {
         <Projects />
       </main>
     </>
-  )
+  );
 }
